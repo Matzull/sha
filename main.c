e3e26034c465d6dde7d6a63ef941e4d6f5556e44
Marcos <marcal16@ucm.es>
2023-05-22 22:17:31 +0200

    init

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <immintrin.h>  // For endianness conversion
+#include <unistd.h>
+#include "sha.h"
+
+int main(int argc, char* argv[]) {
+    int option;
+    uint32_t digest[8];
+    while ((option = getopt(argc, argv, "hs:f:")) != -1) {
+        switch (option) {
+        case 'h':
+            printf("-h show this help\n");
+            printf("-s hash from argument\n");
+            printf("-f hash from file\n");
+            return 0;
+        case 'f':
+            printf("Loading from file\n");
+            break;
+        case 's':
+            sha256_hash((uint8_t*)optarg, digest);
+            print_sha256_hash(digest);
+            break;
+        case 'b':
+            sha256_hash((uint8_t*)optarg, digest);
+            print_sha256_hash(digest);
+            break;
+        default:
+            printf("Opción inválida. Use -h para obtener ayuda.\n");
+            return 1;
+        }
+    }
+}
