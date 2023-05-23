bc87915162f6f4329ca137c5cfcaa100bf220642
Marcos <marcal16@ucm.es>
2023-05-23 15:39:33 +0200

    vis mode

--- main.c
+++ main.c
@@ -5,15 +5,17 @@
 #include <immintrin.h>  // For endianness conversion
 #include <unistd.h>
 #include "sha.h"
+#include "sha_viz.h"
 
 int main(int argc, char* argv[]) {
     int option;
     uint32_t digest[8];
-    while ((option = getopt(argc, argv, "hs:f:")) != -1) {
+    while ((option = getopt(argc, argv, "hs:f:b:")) != -1) {
         switch (option) {
         case 'h':
             printf("-h show this help\n");
             printf("-s hash from argument\n");
+            printf("-b hash from argument\n");
             printf("-f hash from file\n");
             return 0;
         case 'f':
@@ -24,8 +26,8 @@ int main(int argc, char* argv[]) {
             print_sha256_hash(digest);
             break;
         case 'b':
-            sha256_hash((uint8_t*)optarg, digest);
-            print_sha256_hash(digest);
+            v_sha256_hash((uint8_t*)optarg, digest);
+            v_print_sha256_hash(digest);
             break;
         default:
             printf("Opción inválida. Use -h para obtener ayuda.\n");
--- sha_viz.h
+++ sha_viz.h
@@ -5,6 +5,7 @@
 #include <byteswap.h>
 #include <unistd.h>
 #include <immintrin.h>
+#include <time.h>
 
 #define H1 0x6a09e667
 #define H2 0xbb67ae85
@@ -30,7 +31,7 @@
 #define PHI0(x) XOR((RROT(x, 7)), XOR((RROT(x, 18)), (RSHIFT(x, 3))))
 #define PHI1(x) XOR((RROT(x, 17)), XOR((RROT(x, 19)), (RSHIFT(x, 10))))
 
-const uint32_t k[] = {
+const uint32_t v_k[] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -40,7 +41,7 @@ const uint32_t k[] = {
     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
 
-void printBinary(uint8_t *ptr, size_t length)
+void v_printBinary(uint8_t *ptr, size_t length)
 {
     for (size_t i = 0; i < length; i++)
     {
@@ -54,7 +55,40 @@ void printBinary(uint8_t *ptr, size_t length)
     printf("\n");
 }
 
-void printHex(uint32_t *array, size_t length)
+#include <stdint.h>
+#include <stdio.h>
+
+void v_printMemory(uint32_t *ptr, size_t length)
+{
+    uint8_t *bytePtr = (uint8_t *)ptr; // Se interpreta el puntero como un puntero a uint8_t
+
+    for (size_t i = 0; i < length * sizeof(uint32_t); i++)
+    {
+        uint8_t value = bytePtr[i];
+        for (int j = 7; j >= 0; j--)
+        {
+            printf("%d", (value >> j) & 1);
+        }
+        printf(" ");
+    }
+    printf("\n");
+}
+
+void v_print_values(int it, int a, int b, int c, int d, int e, int f, int g, int h)
+{
+    printf("It = %d a = %08x b = %08x c = %08x d = %08x e = %08x f = %08x g = %08x h = %08x\n", it, a, b, c, d, e, f, g, h);
+}
+
+void v_clearScreen()
+{
+    usleep(10000);
+    printf("\033[2J");
+    printf("\033[0;0H");
+    fflush(stdout);
+}
+
+
+void v_printHex(uint32_t *array, size_t length)
 {
     for (size_t i = 0; i < length; i++)
     {
@@ -64,7 +98,7 @@ void printHex(uint32_t *array, size_t length)
 }
 
 // len in bits
-uint8_t* _pad(uint8_t* msg, uint8_t* paddedmsg, uint64_t len, uint32_t* blocks)
+uint8_t* v__pad(uint8_t* msg, uint8_t* paddedmsg, uint64_t len, uint32_t* blocks)
 {
     uint8_t *ptr;
     uint32_t zeroes = 448 - ((len + 1) % 448);
@@ -80,24 +114,26 @@ uint8_t* _pad(uint8_t* msg, uint8_t* paddedmsg, uint64_t len, uint32_t* blocks)
     return paddedmsg;
 }
 
-void _hash(uint32_t blocks, uint8_t* paddedmsg, uint32_t* digest)
+void v__hash(uint32_t blocks, uint8_t* paddedmsg, uint32_t* digest)
 {
     uint32_t a, b, c, d, e, f, g, h;
-    uint32_t Hi1 = H1, Hi2 = H2, Hi3 = H3, Hi4 = H4, Hi5 = H5, Hi6 = H6, Hi7 = H7, Hi8 = H8;
+    uint32_t Hi[8] = {H1, H2, H3, H4, H5, H6, H7, H8};
     uint32_t w[64];
     for (size_t i = 0; i < blocks; i++)
     {
-        a = Hi1;
-        b = Hi2;
-        c = Hi3;
-        d = Hi4;
-        e = Hi5;
-        f = Hi6;
-        g = Hi7;
-        h = Hi8;
-
+        a = Hi[0];
+        b = Hi[1];
+        c = Hi[2];
+        d = Hi[3];
+        e = Hi[4];
+        f = Hi[5];
+        g = Hi[6];
+        h = Hi[7];
         for (size_t j = 0; j < 64; j++)
         {
+            v_clearScreen();
+            v_printMemory(Hi,8);
+            v_print_values(j, a,b,c,d,e,f,g,h);
             uint32_t ch = CH(e, f, g);
             uint32_t maj = MAJ(a, b, c);
             uint32_t sig_0 = SIG0(a);
@@ -112,7 +148,7 @@ void _hash(uint32_t blocks, uint8_t* paddedmsg, uint32_t* digest)
                 w[j] = (PHI1(w[j - 2])) + w[j - 7] + (PHI0(w[j - 15])) + w[j - 16];
             }
 
-            uint32_t t1 = h + sig_1 + ch + k[j] + w[j];
+            uint32_t t1 = h + sig_1 + ch + v_k[j] + w[j];
             uint32_t t2 = sig_0 + maj;
 
             h = g;
@@ -124,36 +160,36 @@ void _hash(uint32_t blocks, uint8_t* paddedmsg, uint32_t* digest)
             b = a;
             a = t1 + t2;
         }
-        Hi1 += a;
-        Hi2 += b;
-        Hi3 += c;
-        Hi4 += d;
-        Hi5 += e;
-        Hi6 += f;
-        Hi7 += g;
-        Hi8 += h;
+        Hi[0] += a;
+        Hi[1] += b;
+        Hi[2] += c;
+        Hi[3] += d;
+        Hi[4] += e;
+        Hi[5] += f;
+        Hi[6] += g;
+        Hi[7] += h;
     }
-    digest[0] = Hi1;
-    digest[1] = Hi2;
-    digest[2] = Hi3;
-    digest[3] = Hi4;
-    digest[4] = Hi5;
-    digest[5] = Hi6;
-    digest[6] = Hi7;
-    digest[7] = Hi8;
+    digest[0] = Hi[0];
+    digest[1] = Hi[1];
+    digest[2] = Hi[2];
+    digest[3] = Hi[3];
+    digest[4] = Hi[4];
+    digest[5] = Hi[5];
+    digest[6] = Hi[6];
+    digest[7] = Hi[7];
 }
 
-void sha256_hash(uint8_t *msg, uint32_t* digest)
+void v_sha256_hash(uint8_t *msg, uint32_t* digest)
 {
     uint8_t *padded = 0;
     uint32_t blocks;
-    padded = _pad(msg, padded, strlen((const char*)msg) << 3, &blocks);
-    _hash(blocks, padded, digest);
+    padded = v__pad(msg, padded, strlen((const char*)msg) << 3, &blocks);
+    v__hash(blocks, padded, digest);
     free(padded);
 }
 
-void print_sha256_hash(uint32_t* digest)
+void v_print_sha256_hash(uint32_t* digest)
 {
-    printHex(digest, 8);
+    v_printHex(digest, 8);
     printf("\n");
 }
